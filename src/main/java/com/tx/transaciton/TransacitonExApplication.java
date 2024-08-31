ec1ea623087b3f83067555b41863ba9bc6af7f97
jeonDev <jjhwhdgus0727@gmail.com>
2024-08-31 12:55:13 +0900

    Initial commit

---
+++ src/main/java/com/tx/transaciton/TransacitonExApplication.java
@@ -0,0 +1,13 @@
+package com.tx.transaciton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacitonExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransacitonExApplication.class, args);
+    }
+
+}
